bb8dac91b2af14e04172a3f947627568ef11c870
Alexander <Partyniceprice@hotmail.se>
2024-05-09 17:34:49 +0200

    Iterated firstpersoncontroller
    Iterated firstpersoncontroller

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody3D
 
 @onready var head = $Head
+@onready var starting_height = head.position.y
 var current_speed = 5.0
 const walking_speed = 5.0
 const sprinting_speed = 10.0
 const crouching_speed = 3.1
 const jump_velocity = 4.5
 const mouse_sense = 0.25
+var lerp_speed = 8.2
+var crouching_lerp_speed = 0.18
+
+var direction = Vector3.ZERO
+@onready var crouching_depth = starting_height - 0.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -16,23 +22,30 @@ func _ready():
 
 
 func _input(event):
-	if event.is_action_pressed("sprint"):
-		current_speed = sprinting_speed
-	else: 
-		current_speed = walking_speed	
-
 	# Mouse
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sense))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sense))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+		get_viewport().set_input_as_handled()
 
-	# Debug stuff
+
+func _shortcut_input(event):
 	if event.is_action_pressed("escape"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
 func _physics_process(delta):
+	if(Input.is_action_pressed("crouch")):
+		current_speed = crouching_speed
+		head.position.y = lerp(head.position.y, crouching_depth, crouching_lerp_speed)
+	else:
+		head.position.y = lerp(head.position.y, starting_height, crouching_lerp_speed)
+		if Input.is_action_pressed("sprint"):
+			current_speed = sprinting_speed
+		else:
+			current_speed = walking_speed
+
 
 	# Add the gravity.
 	if not is_on_floor():
@@ -45,7 +58,9 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
+
+
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
