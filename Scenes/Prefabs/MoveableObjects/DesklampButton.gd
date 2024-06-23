56d5a0c45fb5f92327a2e2162652e414cef773c8
Alexander <Partyniceprice@hotmail.se>
2024-06-23 22:07:37 +0200

    Removed print to console
    Removed print to console

--- Scenes/Prefabs/MoveableObjects/DesklampButton.gd
+++ Scenes/Prefabs/MoveableObjects/DesklampButton.gd
@@ -2,7 +2,5 @@ extends Interactable
 @export var light: SpotLight3D = null
 
 func interact():
-	print("INTERACTED WITH LAMP")
 	if(light != null):
-		print("LAMP WAS NOT NULL")
 		light.visible = !light.visible
