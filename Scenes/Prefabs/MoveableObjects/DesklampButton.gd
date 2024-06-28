9a1d7017b9d923c7aa9d9862bb29c4234150e25a
theteddyz <85722189+theteddyz@users.noreply.github.com>
2024-06-28 23:01:51 +0200

    lamp button

--- Scenes/Prefabs/MoveableObjects/DesklampButton.gd
+++ Scenes/Prefabs/MoveableObjects/DesklampButton.gd
@@ -1,6 +1,11 @@
 extends Interactable
 @export var light: SpotLight3D = null
-
+var button_audio
+@onready var audio_player = $AudioStreamPlayer3D
+func _ready():
+	button_audio = preload("res://Assets/Audio/SoundFX/LampButton2.mp3")
+	audio_player.stream = button_audio
 func interact():
+	audio_player.play()
 	if(light != null):
 		light.visible = !light.visible
