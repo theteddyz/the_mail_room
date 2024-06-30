3674f27668816c612d4a91bd975b0d29e55097eb
theteddyz <85722189+theteddyz@users.noreply.github.com>
2024-06-30 21:12:00 +0200

    really breaking shit now

--- Scripts/LampCollisionDetector.gd
+++ Scripts/LampCollisionDetector.gd
@@ -5,6 +5,7 @@ var collision_threshold = 0.8
 @onready var hinge_joint = $"../JoltHingeJoint3D"
 @onready var lamp_arm = $"../LampArm"
 @onready var lamp_break_sound = $"../AudioStreamPlayer3D"
+@onready var lamp_break_sound_2 = $"../AudioStreamPlayer3D2"
 var broken:bool
 var grabbable_script = preload("res://Scripts/GrabbableObject.gd")
 func _on_lamp_base_body_entered(body):
@@ -23,6 +24,7 @@ func break_lamp():
 	lamp_arm.call("_ready")
 	if !broken:
 		lamp_break_sound.play()
+		lamp_break_sound_2.play()
 		hinge_joint.queue_free()
 		get_parent().apply_impulse(Vector3(0, 10, 0), Vector3(0, 10, 0))
 		lamp_arm.apply_impulse(Vector3(0, 10, 0), Vector3(0, 10, 0))
