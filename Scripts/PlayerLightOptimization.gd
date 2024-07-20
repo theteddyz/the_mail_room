c05c3e9241b7074f904a2b1deb2c8ad79b2beed7
Alexander <Partyniceprice@hotmail.se>
2024-07-20 18:19:59 +0200

    Updated basic lights and meshes to support baking, added first iteration lightmap to first level
    Updated basic lights and meshes to support baking, added first iteration lightmap to first level

--- Scripts/PlayerLightOptimization.gd
+++ Scripts/PlayerLightOptimization.gd
@@ -12,6 +12,22 @@ func _on_area_entered(area):
 	if !lights.is_empty():
 		for light in lights:
 			light.shadow_enabled = true
+			
+			# Apply Shadow Properties
+			var l = light as Light3D
+			l.shadow_bias = 4
+			l.shadow_normal_bias = 1
+			l.shadow_reverse_cull_face = false
+			l.shadow_transmittance_bias = 0.05
+			l.shadow_opacity = 1
+			l.shadow_blur = 1
+			
+			# Distance Fade Properties
+			l.distance_fade_enabled = true
+			l.distance_fade_begin = 50
+			l.distance_fade_shadow = 15.0
+			l.distance_fade_length = 13.45
+			
 			counter += 1
 			print(counter)
 			
