d0a9d7545046e48982846d50ed22caade546290d
Alexander <Partyniceprice@hotmail.se>
2024-08-02 20:13:58 +0200

    Added lightflicker in corridor and monster footsteps on scare 2
    Added lightflicker in corridor and monster footsteps on scare 2

--- Scenes/Worlds/darkroom_scare.gd
+++ Scenes/Worlds/darkroom_scare.gd
@@ -3,12 +3,15 @@ extends Node3D
 @onready var lock_door_area: Area3D = $door_slam_starter
 @onready var door_slam_anim: AnimationPlayer = $AnimationPlayer
 @onready var doorlock = $"../../NavigationRegion3D/Walls/StaticBody3D127/RigidBody3D2/Door_Lock"
-@onready var flickeranimationplayer = $"../../CeilingLights/CeilingLightOn52/AnimationPlayer"
+@onready var flickeranimationplayer:AnimationPlayer = $"../../CeilingLights/CeilingLightOn52/AnimationPlayer"
+@onready var hallwayflickeranimationplayer:AnimationPlayer = $"../../CeilingLights/CeilingLightOn8/FlickeringLight"
+@onready var monster_run_soundplayer:AudioStreamPlayer3D = $MonsterRunSoundPlayer
 var scare_active: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	lock_door_area.monitoring = false
+	hallwayflickeranimationplayer
 	ScareDirector.connect("key_pickedup", activate_scare)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,9 +35,11 @@ func _on_door_slam_starter_body_entered(body):
 		flickeranimationplayer.pause()		
 		flickeranimationplayer.play("RESET")
 		door_slam_anim.play("door_lock")
+		hallwayflickeranimationplayer.play("flicker")
+		monster_run_soundplayer.playing = true
+		var timer = get_tree().create_timer(5.0)
+		timer.timeout.connect(_end_scare)
 
-
-func _on_animation_player_animation_finished(anim_name):
-	if(anim_name == "door_lock"):
-		doorlock.locked = false
-		queue_free()
+func _end_scare():
+	doorlock.locked = false
+	queue_free()
