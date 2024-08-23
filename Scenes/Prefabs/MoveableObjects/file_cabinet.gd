cc2beaf30b708b81896529e6708773187ba4e416
theteddyz <85722189+theteddyz@users.noreply.github.com>
2024-08-23 17:36:15 +0200

    File cabinet color picker is working now

--- Scenes/Prefabs/MoveableObjects/file_cabinet.gd
+++ Scenes/Prefabs/MoveableObjects/file_cabinet.gd
@@ -3,18 +3,23 @@ extends MeshInstance3D
 
 @export_enum("Grey","Dark Green", "Light Green","Beige","Black") var colour: String = "Grey"
 @export_color_no_alpha var color: Color
+@onready var cabinet_1:MeshInstance3D = $"../../Drawer/FileCabinetDrawer_001"
 
-var color2: Array = [
-	preload("res://Assets/Materials/file_cabinet_1.tres"),
-]
+
 var material
+var material_2
 func _ready():
 	material = mesh.surface_get_material(0)
+	material_2 = cabinet_1.mesh.surface_get_material(0)
 	material.albedo_color = color
+	material_2.albedo_color = color
+	cabinet_1.mesh.surface_set_material(0, material_2)
 	mesh.surface_set_material(0, material)
-
 func _process(delta):
 	if Engine.is_editor_hint():
-		print("I'M IN EDITOR")
-	#material.albedo_color = color
-	#material.surface_set_material(0, material)
+		#material.albedo_color = color
+		#material.surface_set_material(0, material)
+		material.albedo_color = color
+		material_2.albedo_color = color
+		mesh.surface_set_material(0,material)
+		cabinet_1.mesh.surface_set_material(0,material_2)
