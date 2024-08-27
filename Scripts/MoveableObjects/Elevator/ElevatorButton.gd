03b98c8369f3b2e8197e99a94ac40ee154ead859
theteddyz <85722189+theteddyz@users.noreply.github.com>
2024-08-27 20:48:00 +0200

    Hightlight added on elevator buttons

--- Scripts/MoveableObjects/Elevator/ElevatorButton.gd
+++ Scripts/MoveableObjects/Elevator/ElevatorButton.gd
@@ -1,6 +1,22 @@
 extends Interactable
 @export var target_scene_path := ""
 @export var floor_num:int
+var text_displayer
+func _ready():
+	EventBus.connect("object_looked_at",on_seen)
+	EventBus.connect("no_object_found",on_unseen)
+	text_displayer = Gui.get_address_displayer()
 func interact():
 	print("GOING TO NEW SCENE ",target_scene_path)
 	EventBus.emitCustomSignal("moved_to_floor", [target_scene_path,floor_num])
+
+
+
+func on_seen(node):
+	if node == self:
+		text_displayer.show_text()
+		text_displayer.set_text(get_parent().name)
+
+func on_unseen(node):
+	if node == self:
+		text_displayer.hide_text()
