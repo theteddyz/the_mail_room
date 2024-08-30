bddeaff1d04a1fa12c591de812c8068ac689d77a
theteddyz <85722189+theteddyz@users.noreply.github.com>
2024-08-30 22:13:11 +0200

    item reader adjustments

--- Scripts/GUI/ItemReader.gd
+++ Scripts/GUI/ItemReader.gd
@@ -1,16 +1,22 @@
 extends Control
-@onready var display_text:RichTextLabel = $RichTextLabel
-
+@onready var display_text:RichTextLabel = $Panel/RichTextLabel
+@onready var panel:Panel = $Panel
 
 
 func display_item(text):
 	if visible == false:
 		display_text.text = "[center]" + text + "[/center]"
+		display_text.call_deferred("scroll_to_line", 0)
+		display_text.call_deferred("_resize_panel")
 		show()
-		print("displaying", display_text.visible)
-		
 
 
 func hide_item():
 	display_text.text = ""
 	hide()
+
+
+func _resize_panel():
+	var text_size = display_text.get_minimum_size()
+	var padding = Vector2(20, 20)
+	panel.rect_size = text_size + padding
