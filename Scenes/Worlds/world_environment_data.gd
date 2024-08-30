a4fd52cf7ff26a17b4715d524a73403e7378aa3d
Alexander <Partyniceprice@hotmail.se>
2024-08-30 18:04:26 +0200

    Set up lightLevelDetector for opening floor
    Set up lightLevelDetector for opening floor

--- Scenes/Worlds/world_environment_data.gd
+++ Scenes/Worlds/world_environment_data.gd
@@ -1,18 +1,19 @@
 extends WorldEnvironment
 
+# Filled with the starting properties for this World_Environment node
 var properties
+
+@export var light_value = 0.069
+@export var dark_value = 0.035
+@export var contrast_darken_factor = 1.2
+@export var saturation_darken_factor = 0.7
 
 func _ready():
 	properties = resource_to_dict(get_environment())
+	GameManager.player_reference.get_node("Neck").get_node("Head").get_node("HeadbopRoot").get_node("LightLevelDetection").we = self
 
 func resource_to_dict(resource: Resource) -> Dictionary:
 	var dict = {}
 	for property_name in resource.get_property_list():
 		dict[property_name.name] = resource.get(property_name.name)
 	return dict
-	
-#func dict_to_resource(dict: Dictionary):
-	#var e = get_environment()
-	#for property_name in e:
-		#e.get(property_name) = dict[property_name]
-	#return dict
