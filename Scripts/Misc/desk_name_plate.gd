8657cbc1728dc1f1d921128dd5c488c89ed05241
theteddyz <85722189+theteddyz@users.noreply.github.com>
2024-08-31 18:33:16 +0200

    Desk name plates added

---
+++ Scripts/Misc/desk_name_plate.gd
@@ -0,0 +1,22 @@
+@tool
+extends Node3D
+
+@export_multiline var name_text:String
+func _process(delta):
+	if Engine.is_editor_hint():
+		update_text()
+
+func _ready():
+	var text_mesh = $RigidBody3D/MeshInstance3D
+	if text_mesh and name_text != text_mesh.mesh.text:
+		var unique_text_mesh = text_mesh.mesh.duplicate()
+		unique_text_mesh.text = name_text
+		text_mesh.mesh = unique_text_mesh
+
+func update_text():
+	if Engine.is_editor_hint():
+		var text_mesh = $RigidBody3D/MeshInstance3D
+		if text_mesh and name_text != text_mesh.mesh.text:
+			var unique_text_mesh = text_mesh.mesh.duplicate()
+			unique_text_mesh.text = name_text
+			text_mesh.mesh = unique_text_mesh
