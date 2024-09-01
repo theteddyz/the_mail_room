680453dc721ef25c9b8b812cc965f099156cf678
theteddyz <85722189+theteddyz@users.noreply.github.com>
2024-09-01 16:26:39 +0200

    Now tweening the readable

--- Scripts/GUI/ItemReader.gd
+++ Scripts/GUI/ItemReader.gd
@@ -1,23 +1,32 @@
 extends Control
-@onready var display_text:RichTextLabel = $Panel/RichTextLabel
-@onready var panel:Panel = $Panel
-
+@onready var display_text:RichTextLabel = $PanelContainer/MarginContainer/VBoxContainer/RichTextLabel
+@onready var panel_container:PanelContainer = $PanelContainer
+var fade_in_tween:Tween
+var fade_out_tween:Tween
 func _ready():
 	visible = false
 func display_item(text):
 	if visible == false:
 		display_text.text = "[center]" + text + "[/center]"
-		display_text.call_deferred("scroll_to_line", 0)
-		display_text.call_deferred("_resize_panel")
 		show()
+		_fade_in()
 
 
 func hide_item():
-	display_text.text = ""
+	_fade_out()
+
+func _fade_in():
+	if fade_out_tween != null:
+		if fade_out_tween.is_running():
+			fade_out_tween.stop()
+	fade_in_tween = create_tween()
+	fade_in_tween.tween_property(panel_container, "modulate:a", 1, 1)
+
+func _fade_out():
+	if fade_in_tween != null:
+		if fade_in_tween.is_running():
+			fade_in_tween.stop()
+	fade_out_tween = create_tween()
+	fade_out_tween.tween_property(panel_container, "modulate:a", 0, 1)
+	await fade_out_tween.finished
 	hide()
-
-
-func _resize_panel():
-	var text_size = display_text.get_minimum_size()
-	var padding = Vector2(20, 20)
-	panel.rect_size = text_size + padding
