930e42f1de59ce43978c4c949bb2fbe992073495
theteddyz <85722189+theteddyz@users.noreply.github.com>
2024-10-01 14:14:19 +0200

    added some more notes to the floor renamed some stuff

--- Scripts/GUI/readable_objectg.gd
+++ Scripts/GUI/readable_objectg.gd
@@ -1,8 +1,8 @@
 extends Area3D
-
 @onready var item_reader
 @export_multiline var object_text:String
 @onready var highlight_mesh:MeshInstance3D = $MeshInstance3D
+var showing:bool = false
 func _ready():
 	item_reader = Gui.get_item_reader()
 	EventBus.connect("object_looked_at",display_text)
@@ -12,8 +12,16 @@ func display_text(node):
 	if node == self:
 		highlight_mesh.show()
 		item_reader.display_item(object_text)
+		showing = true
+	else:
+		if showing:
+			highlight_mesh.hide()
+			item_reader.hide_item()
+			showing = false
+	
 
 func hide_text(node):
 	if node == self:
+		showing = false
 		highlight_mesh.hide()
 		item_reader.hide_item()
