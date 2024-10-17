dcdd874665e8a140ec44c92e118a1cd8022a4c5f
Alexander <Partyniceprice@hotmail.se>
2024-10-17 21:22:20 +0200

    Changed package 5 location and disconnected related-scare
    Changed package 5 location and disconnected related-scare

--- Scenes/Worlds/quiet_corner_scare.gd
+++ Scenes/Worlds/quiet_corner_scare.gd
@@ -11,20 +11,18 @@ var has_been_executed = false
 
 func _ready():
 	monitor_flicker.play("flicker")
-	ScareDirector.connect("package_delivered", activate_scare)
 
-func activate_scare(package_num:int):
-	if package_num == 5:
-		has_been_executed = true	# Variable necessary for all scares, tells other scares which ones have been executed
-		scare_5_vent_sound.playing = false
-		if john_typing_sound_player != null:
-			john_typing_sound_player.playing = false
-		if cubicle_wall_monster != null:
-			cubicle_wall_monster.queue_free()
-		print("SCARE ACTIVATED!")
-		var timer = get_tree().create_timer(4.38)
-		timer.timeout.connect(_end_scare)
-		
+func activate_scare():
+	has_been_executed = true	# Variable necessary for all scares, tells other scares which ones have been executed
+	scare_5_vent_sound.playing = false
+	if john_typing_sound_player != null:
+		john_typing_sound_player.playing = false
+	if cubicle_wall_monster != null:
+		cubicle_wall_monster.queue_free()
+	print("SCARE ACTIVATED!")
+	var timer = get_tree().create_timer(4.38)
+	timer.timeout.connect(_end_scare)
+
 func _end_scare():
 	monitor_flicker.active = false
 	screen_light_2.visible = false
@@ -32,3 +30,7 @@ func _end_scare():
 	monitor.get_node("Monitor_Collision_Handler").destruction_audios.play()
 	monitor.get_node("Interpolator").get_node("ComputerOn").visible = false
 	monitor.get_node("Interpolator").get_node("ComputerOff").visible = true
+
+
+func _on_quiet_scare_starter_body_entered(body: Node3D) -> void:
+	activate_scare()
