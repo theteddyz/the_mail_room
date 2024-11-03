55f91ce61811ae790b5e1de3db8bcae2fa8b4f74
Alexander <Partyniceprice@hotmail.se>
2024-11-03 22:15:15 +0100

    Fixed various lighting issues throughout scenes
    Fixed various lighting issues throughout scenes

--- Scenes/Worlds/world_environment_data.gd
+++ Scenes/Worlds/world_environment_data.gd
@@ -8,17 +8,13 @@ var properties: Dictionary
 @export var light_value = 0.069
 @export var dark_value = 0.035
 
-#DEPRECATED
-#@export var contrast_darken_factor = 1.2
-#@export var saturation_darken_factor = 0.7
-#
-#func _ready():
-	#properties = resource_to_dict(get_environment())
-	#GameManager.player_reference.get_node("Neck").get_node("Head").get_node("HeadbopRoot").get_node("LightLevelDetection").we = self
-	#GameManager.register_world_environment(self)
-#
-#func resource_to_dict(resource: Resource) -> Dictionary:
-	#var dict = {}
-	#for property_name in resource.get_property_list():
-		#dict[property_name.name] = resource.get(property_name.name)
-	#return dict
+func _ready():
+	properties = resource_to_dict(get_environment())
+	GameManager.player_reference.get_node("Neck").get_node("Head").get_node("HeadbopRoot").get_node("LightLevelDetection").we = self
+	GameManager.register_world_environment(self)
+
+func resource_to_dict(resource: Resource) -> Dictionary:
+	var dict = {}
+	for property_name in resource.get_property_list():
+		dict[property_name.name] = resource.get(property_name.name)
+	return dict
