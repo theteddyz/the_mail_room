755d351369502ffc3fa326e45e2bcd92489a5e8f
theteddyz <85722189+theteddyz@users.noreply.github.com>
2025-01-02 17:25:12 +0100

    crt no longer auto starts can actually press the button to play

---
+++ Scripts/Interactable/crt_interactable.gd
@@ -0,0 +1,10 @@
+extends Interactable
+
+@export var video_player:VideoStreamPlayer 
+@export var audio_player:AudioStreamPlayer3D
+
+
+func interact():
+	if !video_player.is_playing():
+		video_player.play()
+		audio_player.play()
