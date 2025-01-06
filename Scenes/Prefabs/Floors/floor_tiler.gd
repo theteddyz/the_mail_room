45f1f183a5c3f66d4e81afe72609904601f0d756
Alexander <Partyniceprice@hotmail.se>
2025-01-06 15:17:52 +0100

    Minor bug fix
    Minor bug fix

--- Scenes/Prefabs/Floors/floor_tiler.gd
+++ Scenes/Prefabs/Floors/floor_tiler.gd
@@ -4,7 +4,6 @@ extends Node3D
 @export var tile_size: Vector2 = Vector2(1, 1):
 	set(new_size):
 		tile_size = new_size
-		update_texture_scale()
 @onready var mesh: MeshInstance3D = $Plane1
 
 func _process(delta):
