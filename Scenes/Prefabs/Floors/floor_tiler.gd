b96c0d17d22fa05574c9b498499131125f8a7788
Alexander <Partyniceprice@hotmail.se>
2025-01-27 21:19:10 +0100

    Adjusted project settings, bugfixed floor_tiler, added hr_lighting profile for prefabbed lights

--- Scenes/Prefabs/Floors/floor_tiler.gd
+++ Scenes/Prefabs/Floors/floor_tiler.gd
@@ -1,9 +1,12 @@
 @tool 
 extends Node3D
 @export var active: bool = false
-@export var tile_size: Vector2 = Vector2(1, 1):
-	set(new_size):
-		tile_size = new_size
+@export var tile_size: Vector2 = Vector2(1, 1)
+var internal_tile_size: Vector2
+
+func _ready():
+	internal_tile_size = tile_size
+	update_texture_scale()
 
 func _process(delta):
 	# Ensure the script works in the editor as well as runtime
@@ -12,6 +15,6 @@ func _process(delta):
 
 func update_texture_scale():
 	var mesh_scale = transform.basis.get_scale()
-	var uv_scale = Vector2(mesh_scale.x / tile_size.x, mesh_scale.z / tile_size.y)
+	var uv_scale = Vector2(mesh_scale.x / internal_tile_size.x, mesh_scale.z / internal_tile_size.y)
 	$Plane1.mesh.surface_get_material(0).uv1_scale.x = uv_scale.x
 	$Plane1.mesh.surface_get_material(0).uv1_scale.y = uv_scale.y
