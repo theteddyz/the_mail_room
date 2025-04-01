773ce94abfb1b408adb76cbc2644c66dded9624d
Alexander <Partyniceprice@hotmail.se>
2025-04-01 21:59:01 +0200

    Re-made ratman vent (HR)
    Re-made ratman vent (HR)

--- Scenes/Worlds/spinning_fan.gd
+++ Scenes/Worlds/spinning_fan.gd
@@ -1,3 +1,4 @@
 extends Node3D
+@export var axis: Vector3 = Vector3(0, 0, 1)
 func _physics_process(delta: float) -> void:
-	rotate_z(deg_to_rad(225) * delta)
+	rotate_object_local(axis, deg_to_rad(225) * delta)
