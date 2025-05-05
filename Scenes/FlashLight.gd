bc7db59b45cdda26ceb1e1676620c648ec52b79f
jabbathefrukt <nilsdaniellagula@gmail.com>
2025-05-05 18:24:17 +0200

    Updates to Flashlight and Shaders

--- Scenes/FlashLight.gd
+++ Scenes/FlashLight.gd
@@ -5,9 +5,25 @@ extends Node3D
 @export var omni_light: OmniLight3D
 @export var audio: AudioStreamPlayer
 @export var distance: float = 1.0
+var is_on: bool = false
+func _ready() -> void:
+	omni_light.visible = false
+	spot_light.visible = false
+
+func _input(event):
+
+	handle_keyboard_press(event)
 
 func handle_keyboard_press(event: InputEvent):
 	if event.is_action_pressed("drive"):
+		if(is_on):
+			is_on = false
+			omni_light.visible = false
+			spot_light.visible = false
+		else:
+			omni_light.visible = true
+			spot_light.visible = true
+			is_on = true
 		audio.play()	
 
 func _physics_process(delta: float) -> void:
