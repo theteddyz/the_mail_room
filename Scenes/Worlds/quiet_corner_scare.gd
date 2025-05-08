c1cdb715b8e6cbaa744ba97f60687db8570b8a29
Alexander <Partyniceprice@hotmail.se>
2025-05-08 22:17:40 +0200

    Fixed quiet corner scare
    Fixed quiet corner scare

--- Scenes/Worlds/quiet_corner_scare.gd
+++ Scenes/Worlds/quiet_corner_scare.gd
@@ -4,23 +4,15 @@ var scare_index = 5
 var has_been_executed = false
 @onready var scare_5_vent_sound: AudioStreamPlayer3D = $"../../Scare5VentSound"
 @onready var john_typing_sound_player: AudioStreamPlayer3D = $"../CUBICLE SCARE/JohnTypingSoundPlayer"
-@onready var cubicle_wall_monster: StaticBody3D = $"../../Cubicle_Door"
-@onready var monitor: RigidBody3D = $"../../NavigationRegion3D/Objects/Desk37/GrabableObjectTemplate/Monitor"
-@onready var screen_light_2: SpotLight3D = $"../../NavigationRegion3D/Objects/Desk37/GrabableObjectTemplate/Monitor/ComputerOn/ScreenLight"
-
+@onready var cubicle_wall: StaticBody3D = $"../../Cubicle_Door"
+@onready var monitor: RigidBody3D = $"../../NavigationRegion3D/Objects/Desk37/GrabableObjectTemplate"
+@onready var screen_light_2: SpotLight3D = $"../../NavigationRegion3D/Objects/Desk37/GrabableObjectTemplate/ComputerOn/ScreenLight"
 @onready var monitor_flicker: AnimationPlayer = $"../../NavigationRegion3D/Objects/Desk37/monitor_flicker"
-
-func _ready():
-	monitor_flicker.play("flicker")
 
 func activate_scare():
 	ScareDirector.emit_signal("scare_activated", scare_index)
 	has_been_executed = true	# Variable necessary for all scares, tells other scares which ones have been executed
 	scare_5_vent_sound.playing = false
-	if john_typing_sound_player != null:
-		john_typing_sound_player.playing = false
-	if cubicle_wall_monster != null:
-		cubicle_wall_monster.queue_free()
 	print("SCARE ACTIVATED!")
 	var timer = get_tree().create_timer(4.38)
 	timer.timeout.connect(_end_scare)
@@ -32,7 +24,6 @@ func _end_scare():
 	monitor.get_node("Monitor_Collision_Handler").destruction_audios.play()
 	monitor.get_node("ComputerOn").visible = false
 	monitor.get_node("ComputerOff").visible = true
-
 
 func _on_quiet_scare_starter_body_entered(_body: Node3D) -> void:
 	activate_scare()
